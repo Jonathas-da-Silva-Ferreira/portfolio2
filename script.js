f78ff6ded706f6aef80f43f4b69bd3193b8fb468
Jonathas da Silva Ferreira <jonathasx.525@gmail.com>
2024-04-25 11:24:03 -0300

    atualizado

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const header = document.querySelector("header");
 
-window.addEventListener("sroll" , function(){
+window.addEventListener("scroll", function() {
     header.classList.toggle("sticky", window.scrollY > 70);
 });
